971dfb25488a90d6695e31416433e144d846e4b3
sistines <kerryphisher234@outlook.com>
2022-09-20 22:19:15 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+author = "Sistines"
+
+
+class Entity:
+    def __init__(self, health, dmg):
+        self.health = health
+        self.dmg = dmg
+
+
+class Player(Entity):
+    def __init__(self):
+        super().__init__()
+
+
+print(f"{author}'s Text Adventure")
