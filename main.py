e54fb95f3d6e1d9cc5f366ff8a9ee195f3865a09
sistines <kerryphisher234@outlook.com>
2022-09-21 14:41:15 +1000

    first progress of today

--- main.py
+++ main.py
@@ -11,5 +11,29 @@ class Player(Entity):
     def __init__(self):
         super().__init__()
 
+    def move(self, direction):
+        pass
 
-print(f"{author}'s Text Adventure!")
+
+class Enemy(Entity):
+    def __init__(self):
+        super().__init__()
+
+
+class Game():
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+
+def main() -> None:
+    print(f"Welcome to {author}'s Text Adventure!")
+    name = input('Name: ')
+    game = Game(name)
+    print(f"Welcome {name}")
+
+
+if __name__ == "__main__":
+    main()
